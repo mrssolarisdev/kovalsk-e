53d71b9534d0343b554d3e6e5373c60f4c7c3213
Marcela <mrssolaris.dev@gmail.com>
2022-12-05 21:02:13 -0300

    Updating webdriver so there is no need of installing it

--- core/warren/test.py
+++ core/warren/test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from config import settings
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(executable_path=settings.scrapper.WEBDRIVER_PATH)
-driver.get('https://google.com')
+driver_service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=driver_service)
+driver.get('https://www.mercadolivre.com/jms/mlb/lgz/msl/login/H4sIAAAAAAAEAy2OQQ7DIBAD_-IzSu4c-xG0SRaCCgEtm5Kq6t8r2h4t22O_kEqIh9NnZVjwVVNco8KgJlJfJLu4wSJXGLSo_JdpGRESyqwsDfY1QIG3G_siA6VyMgzo1N35VDrsbwoGsTm-lOWg5Dovj8jD9ZTaaIQCi121NjvPvfcps6y0lUqhTGvJ0yIzDIRDbMrC49537G3gqalTofUO--W9P0BZ4FvhAAAA/user')
